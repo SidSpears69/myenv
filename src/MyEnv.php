4dc1c51602ae8ce5233231a6cdeafbad62b04e3b
SidSpears <sidspears55@gmail.com>
2020-07-16 20:16:41 +0300

    правки

--- src/MyEnv.php
+++ src/MyEnv.php
@@ -2,12 +2,32 @@
 
 namespace sidspears69\myenv;
 
+use Dotenv\Dotenv;
+
 class MyEnv
 {
+    private static function loadEnv($dir) {
+        var_dump($dir);
+        $dotenv = Dotenv::createImmutable($dir);
+        $dotenv->load();
+    }
     public static function get($stroke) {
         $dir = $_SERVER['DOCUMENT_ROOT'];
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
+        self::findAllEnv($dir);
         return $_ENV[$stroke];
+    }
+    private static function findAllEnv($dir) {
+        if(file_exists($dir."/.env")) {
+            self::loadEnv($dir);
+        };
+        foreach(glob($dir . '/*/') as $files) {
+            var_dump($files);
+            if (!pathinfo($files, PATHINFO_EXTENSION)) {
+                self::findAllEnv($files);
+            }
+            else {
+                return;
+            }
+        }
     }
 }
